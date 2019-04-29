681bbd0b6bffb2129c48ba61895f786b92dd5d2b
davidtlee <davidtlee@gmail.com>
2019-04-28 17:51:52 -0700

    text-slide: generate initial component files

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -13,6 +13,7 @@ import { DashboardEventsEffects } from './dashboard/+events/dashboard.events.eff
 import { DashboardStateEffects } from './dashboard/+state/dashboard.state.effects';
 
 // Components
+import { TextSlideComponent } from './reorient/slides/text-slide/text-slide.component';
 import { LongTermGoalsSlideComponent } from './reorient/slides/long-term-goals-slide/long-term-goals-slide.component';
 import { QuarterGoalsSlideComponent } from './reorient/slides/quarter-goals-slide/quarter-goals-slide.component';
 import { ProgressBarComponent } from './reorient/progress-bar/progress-bar.component';
@@ -45,6 +46,7 @@ import { DateTimeComponent } from './dashboard/date-time/date-time.component';
     ReorientComponent,
     DashboardComponent,
     // Components
+    TextSlideComponent,
     LongTermGoalsSlideComponent,
     QuarterGoalsSlideComponent,
     ProgressBarComponent,
