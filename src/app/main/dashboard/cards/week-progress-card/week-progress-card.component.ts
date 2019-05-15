7c5c3bc29fbf51c9d6cac6464761213f5547740e
jansonchiu <21264729+jansonchiu@users.noreply.github.com>
2019-05-15 12:15:47 -0700

    Testing

--- src/app/main/dashboard/cards/week-progress-card/week-progress-card.component.ts
+++ src/app/main/dashboard/cards/week-progress-card/week-progress-card.component.ts
@@ -16,14 +16,36 @@ export class WeekProgressCardComponent implements OnInit {
   @Input() plans: WeekGoalProgress[];
 
   // --------------- LOCAL UI STATE ----------------------
- 
+
 
   constructor() { }
 
   ngOnInit() {
     console.log(this.plans);
   }
-
+  getMaxTime(){
+    var max = 0;
+    for( let time of this.plans){
+        if(time.totalAllocatedMins > max){
+          max = time.totalAllocatedMins;
+        }
+    }
+    return max;
+  }
+  hours(ind){
+    return Math.floor(this.plans[ind].totalAllocatedMins/60);
+  }
+  lightSize(ind){
+    var maxSize = this.getMaxTime();
+    if( this.plans[ind].totalAllocatedMins < maxSize){
+      return Math.floor((this.plans[ind].totalAllocatedMins/maxSize)*100)+'%';
+    }else{
+      return 75 +'%';
+    }
+  }
+  boldSize(ind){
+    return Math.floor((this.plans[ind].totalCompletedMins/this.plans[ind].totalAllocatedMins)*100)+'%';
+  }
 
   // --------------- DATA BINDING FUNCTIONS --------------
 
