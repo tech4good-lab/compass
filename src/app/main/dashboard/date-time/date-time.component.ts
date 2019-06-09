c1a52272fd73ce129aac4e53f8e2ff2325a7c669
scheng14 <51351732+scheng14@users.noreply.github.com>
2019-06-10 00:07:16 +0800

    finish date time data.

--- src/app/main/dashboard/date-time/date-time.component.ts
+++ src/app/main/dashboard/date-time/date-time.component.ts
@@ -13,17 +13,27 @@ export class DateTimeComponent implements OnInit {
 
   /** The current time. */
   @Input() time: Date;
+  monthNames: string[];
 
   // --------------- LOCAL UI STATE ----------------------
  
 
-  constructor() { }
+  constructor() {
+    this.monthNames =  ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    
+  }
+  //2019-06-09T08:22:47.772Z
 
   ngOnInit() {
+    console.log(this.time);
+    console.log(this.time.getDate());
+
   }
 
   // --------------- DATA BINDING FUNCTIONS --------------
-
+  formattedDate() {
+    return this.monthNames[this.time.getMonth()] + " " + this.time.getDate() + "th, " + this.time.getFullYear();
+  }
 
   // --------------- EVENT BINDING FUNCTIONS -------------
 
