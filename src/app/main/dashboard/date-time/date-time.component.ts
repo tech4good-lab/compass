ad8ab837901eeffe8af4b24f58f3016165534246
Janson <jansonchiu343@gmail.com>
2019-07-19 15:11:23 -0700

    finish data

--- src/app/main/dashboard/date-time/date-time.component.ts
+++ src/app/main/dashboard/date-time/date-time.component.ts
@@ -24,7 +24,17 @@ export class DateTimeComponent implements OnInit {
   }
 
   // --------------- DATA BINDING FUNCTIONS --------------
-  
+
+  /** Function to determine which suffix */
+  calcSuffix(day) {
+    if (day > 3 && day < 21) return 'th'; 
+    switch (day % 10) {
+      case 1:  return "st";
+      case 2:  return "nd";
+      case 3:  return "rd";
+      default: return "th";
+    }
+  }
 
   // --------------- EVENT BINDING FUNCTIONS -------------
 
