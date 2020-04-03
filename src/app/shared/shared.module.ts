8e20101ff6058f1631ac5676c8e179baa8db196d
Aidan Nguyen <anguyenkn@gmail.com>
2020-04-03 12:38:40 -0700

    updated package.json file

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -11,7 +11,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
-import { MatSnackBarModule } from '@angular/material';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDialogModule } from '@angular/material/dialog';
