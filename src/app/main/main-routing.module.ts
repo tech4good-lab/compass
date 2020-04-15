94edce1232a7d66c8f984359eafe425b13d79c97
bjpaulse <bjpaulse@ucsc.edu>
2020-04-15 12:21:36 -0700

    change sandbox route to go to new-quarter component

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -4,8 +4,10 @@ import { Routes, RouterModule } from '@angular/router';
 // Containers
 import { ReorientComponent } from './reorient/reorient.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { NewQuarterComponent } from './dashboard/new-quarter/new-quarter.component';
 
 const routes: Routes = [
+  { path: 'new-quarter', component: NewQuarterComponent },
   { path: 'reorient', component: ReorientComponent },
   { path: 'dashboard', component: DashboardComponent },
 ];
