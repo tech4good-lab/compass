fb29c2dcd625d3f1d03a469b6c9995c0a1aa86a6
mlpierce22 <mlpierce22@gmail.com>
2020-04-29 15:18:10 -0700

    Routing setup for new week

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -4,8 +4,10 @@ import { Routes, RouterModule } from '@angular/router';
 // Containers
 import { ReorientComponent } from './reorient/reorient.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { NewWeekComponent } from './dashboard/new-week/new-week.component';
 
 const routes: Routes = [
+  { path: 'new-week', component: NewWeekComponent },
   { path: 'reorient', component: ReorientComponent },
   { path: 'dashboard', component: DashboardComponent },
 ];
