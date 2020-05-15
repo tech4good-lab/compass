312f2e5f7768a51ab6f095e7fb576a2e2d0bb70a
m-ortizfischer <36421193+m-ortizfischer@users.noreply.github.com>
2020-05-14 18:57:58 -0700

    completed

--- src/app/sandbox-routing.module.ts
+++ src/app/sandbox-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '**', redirectTo: 'completed', pathMatch: 'full' },
+  { path: '**', redirectTo: 'new-quarter', pathMatch: 'full' },
 ];
 
 @NgModule({
