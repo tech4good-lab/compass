2330cd85e9134c03bf4d75a98eec83c226316773
Conrad Pereira <60755227+ConradP3@users.noreply.github.com>
2020-05-22 19:26:10 -0700

    completed

--- src/app/main/dashboard/cards/upcoming-card/upcoming-card.component.ts
+++ src/app/main/dashboard/cards/upcoming-card/upcoming-card.component.ts
@@ -1,33 +1,37 @@
-import { Component, OnInit, ChangeDetectionStrategy, Input, Output, EventEmitter } from '@angular/core';
-import { UpcomingEventsData } from '../../+state/dashboard.model';
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  Input,
+  Output,
+  EventEmitter
+} from "@angular/core";
+import { UpcomingEventsData } from "../../+state/dashboard.model";
 
 /** Displays the upcoming events. */
 @Component({
-  selector: 'app-upcoming-card',
-  templateUrl: './upcoming-card.component.html',
-  styleUrls: ['./upcoming-card.component.scss'],
+  selector: "app-upcoming-card",
+  templateUrl: "./upcoming-card.component.html",
+  styleUrls: ["./upcoming-card.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UpcomingCardComponent implements OnInit {
-
   // --------------- INPUTS AND OUTPUTS ------------------
 
   /** Upcoming events. */
   @Input() events: UpcomingEventsData;
 
   // --------------- LOCAL UI STATE ----------------------
- 
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    console.log(this.events);
   }
 
   // --------------- DATA BINDING FUNCTIONS --------------
 
-
   // --------------- EVENT BINDING FUNCTIONS -------------
-
 
   // --------------- OTHER -------------------------------
 }
