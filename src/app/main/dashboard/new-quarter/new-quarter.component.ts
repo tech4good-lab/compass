e6f50826c093c8c7f475ce8dbf1b6cb65622d149
jack-yifei-wang <53378207+jack-yifei-wang@users.noreply.github.com>
2020-05-26 09:16:37 -0700

    completed

--- src/app/main/dashboard/new-quarter/new-quarter.component.ts
+++ src/app/main/dashboard/new-quarter/new-quarter.component.ts
@@ -1,32 +1,44 @@
-import { Component, OnInit, ChangeDetectionStrategy, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  Input,
+  Output,
+  EventEmitter
+} from "@angular/core";
 
 /** Prompt to reorient at the beginning of a new quarter. */
 @Component({
-  selector: 'app-new-quarter',
-  templateUrl: './new-quarter.component.html',
-  styleUrls: ['./new-quarter.component.scss'],
+  selector: "app-new-quarter",
+  templateUrl: "./new-quarter.component.html",
+  styleUrls: ["./new-quarter.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NewQuarterComponent implements OnInit {
-
   // --------------- INPUTS AND OUTPUTS ------------------
- 
+
   /** Events for initiating quarterly reorientation. */
   @Output() reorient: EventEmitter<void> = new EventEmitter<void>();
 
   // --------------- LOCAL UI STATE ----------------------
- 
+  //@Input() message: ChangeDetectionStrategy;
+  header_text:string = "It's a new quarter!";
+  header_text_2: string = "Time to set some new goals.";
+  button_text:string = "Get started";
 
-  constructor() { }
+
+  constructor() {
+
+  }
 
   ngOnInit() {
+    
   }
 
   // --------------- DATA BINDING FUNCTIONS --------------
 
 
   // --------------- EVENT BINDING FUNCTIONS -------------
-
 
   // --------------- OTHER -------------------------------
 }
