62162d8b48e5c29e5c407df2d3b3cc58b56dfa5f
ivyphan <65368690+ivyphan@users.noreply.github.com>
2020-05-28 14:57:26 -0700

    completed

--- src/app/main/dashboard/new-quarter/new-quarter.component.ts
+++ src/app/main/dashboard/new-quarter/new-quarter.component.ts
@@ -1,32 +1,11 @@
-import { Component, OnInit, ChangeDetectionStrategy, Input, Output, EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
 
-/** Prompt to reorient at the beginning of a new quarter. */
 @Component({
-  selector: 'app-new-quarter',
-  templateUrl: './new-quarter.component.html',
-  styleUrls: ['./new-quarter.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'app-new-quarter',
+  templateUrl: './new-quarter.component.html',
+  styleUrls: ['./new-quarter.component.scss'],
+
 })
-export class NewQuarterComponent implements OnInit {
-
-  // --------------- INPUTS AND OUTPUTS ------------------
- 
-  /** Events for initiating quarterly reorientation. */
-  @Output() reorient: EventEmitter<void> = new EventEmitter<void>();
-
-  // --------------- LOCAL UI STATE ----------------------
- 
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-  // --------------- DATA BINDING FUNCTIONS --------------
-
-
-  // --------------- EVENT BINDING FUNCTIONS -------------
-
-
-  // --------------- OTHER -------------------------------
+export class NewQuarterComponent implements OnInit {
 }
+
