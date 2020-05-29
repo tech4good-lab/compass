d9e7db427db7b324f2999f891dea612736f0116a
cjmckinnon <65366559+cjmckinnon@users.noreply.github.com>
2020-05-29 15:51:12 -0700

    completed

--- src/app/main/dashboard/cards/upcoming-card/upcoming-card.component.ts
+++ src/app/main/dashboard/cards/upcoming-card/upcoming-card.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ChangeDetectionStrategy, Input, Output, EventEmitter } from '@angular/core';
 import { UpcomingEventsData } from '../../+state/dashboard.model';
-
+ 
 /** Displays the upcoming events. */
 @Component({
   selector: 'app-upcoming-card',
@@ -9,25 +9,26 @@ import { UpcomingEventsData } from '../../+state/dashboard.model';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UpcomingCardComponent implements OnInit {
-
+ 
   // --------------- INPUTS AND OUTPUTS ------------------
-
+ 
   /** Upcoming events. */
   @Input() events: UpcomingEventsData;
-
+ 
   // --------------- LOCAL UI STATE ----------------------
  
-
+ 
   constructor() { }
-
+ 
   ngOnInit() {
+    console.log(this.events)
   }
-
+ 
   // --------------- DATA BINDING FUNCTIONS --------------
-
-
+ 
+ 
   // --------------- EVENT BINDING FUNCTIONS -------------
-
-
+ 
+ 
   // --------------- OTHER -------------------------------
 }
