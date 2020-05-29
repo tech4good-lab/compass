fc85119975c062fef729f67a76a8aa18248bd0fd
nshak <nshak@ucsc.edu>
2020-05-28 17:28:18 -0700

    completed

--- src/app/main/dashboard/cards/week-goals-card/week-goals-card.component.ts
+++ src/app/main/dashboard/cards/week-goals-card/week-goals-card.component.ts
@@ -1,17 +1,23 @@
-import { Component, OnInit, ChangeDetectionStrategy, Input, Output, EventEmitter } from '@angular/core';
-import { WeekGoal } from '../../../../core/store/week-goal/week-goal.model';
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  Input,
+  Output,
+  EventEmitter
+} from "@angular/core";
+import { WeekGoal } from "../../../../core/store/week-goal/week-goal.model";
 
 /** Displays the weekly goals. */
 @Component({
-  selector: 'app-week-goals-card',
-  templateUrl: './week-goals-card.component.html',
-  styleUrls: ['./week-goals-card.component.scss'],
+  selector: "app-week-goals-card",
+  templateUrl: "./week-goals-card.component.html",
+  styleUrls: ["./week-goals-card.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class WeekGoalsCardComponent implements OnInit {
-
   // --------------- INPUTS AND OUTPUTS ------------------
-  
+
   /** The start of the week. */
   @Input() startOfWeek: Date;
 
@@ -19,21 +25,34 @@ export class WeekGoalsCardComponent implements OnInit {
   @Input() goals: WeekGoal[];
 
   /** Edit weekly goals events. */
-  @Output() editGoals: EventEmitter<WeekGoal[]> = new EventEmitter<WeekGoal[]>();
+  @Output() editGoals: EventEmitter<WeekGoal[]> = new EventEmitter<
+    WeekGoal[]
+  >();
 
   // --------------- LOCAL UI STATE ----------------------
- 
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   // --------------- DATA BINDING FUNCTIONS --------------
 
+  getEndOfWeek() {
+    let curr = new Date();
+    let lastDay = new Date(curr.setDate(this.startOfWeek.getDate() + 7));
+    return lastDay;
+  }
+
+  getGoals() {
+    console.log(this.goals);
+  }
 
   // --------------- EVENT BINDING FUNCTIONS -------------
 
+  replaceGoals() {
+    console.log('Replace goals');
+    this.editGoals.emit();
+  }
 
   // --------------- OTHER -------------------------------
 }
