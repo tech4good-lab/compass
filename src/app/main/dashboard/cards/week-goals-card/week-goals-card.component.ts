20a03a6df217713274335547790f7a66caa2140b
Randall Lowe <safelock@users.noreply.github.com>
2020-05-29 09:56:35 -0700

    completed

--- src/app/main/dashboard/cards/week-goals-card/week-goals-card.component.ts
+++ src/app/main/dashboard/cards/week-goals-card/week-goals-card.component.ts
@@ -22,15 +22,32 @@ export class WeekGoalsCardComponent implements OnInit {
   @Output() editGoals: EventEmitter<WeekGoal[]> = new EventEmitter<WeekGoal[]>();
 
   // --------------- LOCAL UI STATE ----------------------
- 
-
+  goalTitle:string = "This Week's Goals";
+  //dateTitle:string = "9/24 - 9/30";
+  bulletList:string[] = ['Finish Google cover letter', 'Apply to Microsoft', 'Practice implementing data structures'];
+  listItem:string = this.bulletList[0];
+  pencilIcon:string = "https://i.imgur.com/FTl1OYy.png";
   constructor() { }
 
   ngOnInit() {
+    console.log(this.startOfWeek)
+    console.log(this.goals)
   }
-
+ 
   // --------------- DATA BINDING FUNCTIONS --------------
+  /*this function grabs the current start date of the week (month and day) from console
+      and adds 6 days onto the variable "last date" of the week and returns 
+      the last day of the week. We then will have our dates of the week in a proper 
+      format after html doc has run */
+      
+  getOurDates(){
+    var curDay = this.startOfWeek.getDate();
+    var curMonth = this.startOfWeek.getMonth();
+    var lastDate = curDay + 6;
 
+    var newDate = new Date(2020, curMonth, lastDate, 0,0,0,0);
+    return newDate;
+  }
 
   // --------------- EVENT BINDING FUNCTIONS -------------
 
