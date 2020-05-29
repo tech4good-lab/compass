8aef729141f786dc049a5036f1f3d7d6705a59a3
Karishma Shah <karishmashah@me.com>
2020-05-29 16:15:28 -0700

    completed

--- src/app/main/dashboard/cards/upcoming-card/upcoming-card.component.ts
+++ src/app/main/dashboard/cards/upcoming-card/upcoming-card.component.ts
@@ -21,8 +21,40 @@ export class UpcomingCardComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    console.log(this.events);
+  }
+  getDate(ts) {
+    var date = new Date(ts*1000);
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var hourString = hours + "";
+    var suffixString = "AM"
+    if(hours >= 12){
+      if (hours > 12){
+        hours = hours - 12;
+      }
+      hourString = hours + "";
+      suffixString = "PM"
+    }
+    var minuteString = minutes + "";
+    if (minutes < 10) {
+      minuteString = "0" + minutes + "";
+    }
+    //return hourString + ":" + minuteString + "" + suffixString;
+    return hourString + suffixString;
   }
 
+  randomGradient() {
+    var randomColor1 = Math.floor(Math.random()*16777215).toString(16);
+    var white = 0xffffff;
+    return "linear-gradient(90.81deg, #" + randomColor1.toString() + " -3.96%, #" + white.toString() + " 130.33%)"
+  }
+
+  removeSpaces(st) {
+    var kwArray = st.split(" ");
+    var word = kwArray.join("");
+    return word;
+  }
   // --------------- DATA BINDING FUNCTIONS --------------
 
 
