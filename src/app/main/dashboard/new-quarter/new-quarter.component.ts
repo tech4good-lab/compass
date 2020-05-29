c3db644d3e5ed744e6e82fda09e40b92faf84a39
Anirudh Ahuja <anahuja@ucsc.edu>
2020-05-28 21:13:20 -0700

    completed

--- src/app/main/dashboard/new-quarter/new-quarter.component.ts
+++ src/app/main/dashboard/new-quarter/new-quarter.component.ts
@@ -13,11 +13,17 @@ export class NewQuarterComponent implements OnInit {
  
   /** Events for initiating quarterly reorientation. */
   @Output() reorient: EventEmitter<void> = new EventEmitter<void>();
-
+  
+  
   // --------------- LOCAL UI STATE ----------------------
- 
+  imageUrl:string = "https://i.imgur.com/PmiIjUC.png"
+  titleText:string = "It's a new quarter!";
+  subText:string = "Time to set some new goals.";
+  gButton:string = "Get started";
+  
+  constructor() {    
 
-  constructor() { }
+  }
 
   ngOnInit() {
   }
