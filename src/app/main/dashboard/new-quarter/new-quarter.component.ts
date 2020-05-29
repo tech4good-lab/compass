69e70c3091b085f2aafab48216f1aa9a21faa239
Hayato1 <65315938+Hayato1@users.noreply.github.com>
2020-05-28 18:26:01 -0700

    completed

--- src/app/main/dashboard/new-quarter/new-quarter.component.ts
+++ src/app/main/dashboard/new-quarter/new-quarter.component.ts
@@ -1,32 +1,41 @@
-import { Component, OnInit, ChangeDetectionStrategy, Input, Output, EventEmitter } from '@angular/core';
-
-/** Prompt to reorient at the beginning of a new quarter. */
-@Component({
-  selector: 'app-new-quarter',
-  templateUrl: './new-quarter.component.html',
-  styleUrls: ['./new-quarter.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
-})
-export class NewQuarterComponent implements OnInit {
-
-  // --------------- INPUTS AND OUTPUTS ------------------
- 
-  /** Events for initiating quarterly reorientation. */
-  @Output() reorient: EventEmitter<void> = new EventEmitter<void>();
-
-  // --------------- LOCAL UI STATE ----------------------
- 
-
-  constructor() { }
-
-  ngOnInit() {
+import {
+    Component,
+    OnInit,
+    ChangeDetectionStrategy,
+    Input,
+    Output,
+    EventEmitter
+  } from "@angular/core";
+  
+  /** Prompt to reorient at the beginning of a new quarter. */
+  @Component({
+    selector: "app-new-quarter",
+    templateUrl: "./new-quarter.component.html",
+    styleUrls: ["./new-quarter.component.scss"],
+    changeDetection: ChangeDetectionStrategy.OnPush
+  })
+  export class NewQuarterComponent implements OnInit {
+    // --------------- INPUTS AND OUTPUTS ------------------
+    quarter_headline:string;
+    goals:string;
+    buttontext:string;
+    /** Events for initiating quarterly reorientation. */
+      
+  
+    // --------------- LOCAL UI STATE ----------------------
+  
+    constructor() {
+      this.quarter_headline = "It's a new quarter!";
+      this.goals = "Time to set some new goals.";
+      this.buttontext = "Get started";
+    }
+  
+    ngOnInit() {}
+  
+    // --------------- DATA BINDING FUNCTIONS --------------
+  
+    // --------------- EVENT BINDING FUNCTIONS -------------
+  
+    // --------------- OTHER -------------------------------
   }
-
-  // --------------- DATA BINDING FUNCTIONS --------------
-
-
-  // --------------- EVENT BINDING FUNCTIONS -------------
-
-
-  // --------------- OTHER -------------------------------
-}
+  
