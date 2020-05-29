3a46745e05fb3c95da56ceee460c8cc090de4b32
cjmckinnon <65366559+cjmckinnon@users.noreply.github.com>
2020-05-29 15:47:24 -0700

    completed

--- src/app/main/dashboard/cards/week-goals-card/week-goals-card.component.ts
+++ src/app/main/dashboard/cards/week-goals-card/week-goals-card.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ChangeDetectionStrategy, Input, Output, EventEmitter } from '@angular/core';
 import { WeekGoal } from '../../../../core/store/week-goal/week-goal.model';
-
+ 
 /** Displays the weekly goals. */
 @Component({
   selector: 'app-week-goals-card',
@@ -9,31 +9,41 @@ import { WeekGoal } from '../../../../core/store/week-goal/week-goal.model';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class WeekGoalsCardComponent implements OnInit {
-
+ 
   // --------------- INPUTS AND OUTPUTS ------------------
   
   /** The start of the week. */
   @Input() startOfWeek: Date;
-
+ 
   /** Goals for a week. */
   @Input() goals: WeekGoal[];
 
+ 
   /** Edit weekly goals events. */
   @Output() editGoals: EventEmitter<WeekGoal[]> = new EventEmitter<WeekGoal[]>();
+ 
 
   // --------------- LOCAL UI STATE ----------------------
  
-
+ 
   constructor() { }
+ 
+ 
+  // --------------- DATA BINDING FUNCTIONS --------------
+  endOfWeek: Date;
 
   ngOnInit() {
+    this.endOfWeek = new Date();
+    this.endOfWeek.setDate( this.startOfWeek.getDate() + 6 );
   }
-
-  // --------------- DATA BINDING FUNCTIONS --------------
-
-
+  
   // --------------- EVENT BINDING FUNCTIONS -------------
+  /** Function for emmiting an edit goals */
+  edit() {
 
-
+    console.log("Editing Now!");
+    this.editGoals.emit();
+  }
+ 
   // --------------- OTHER -------------------------------
 }
