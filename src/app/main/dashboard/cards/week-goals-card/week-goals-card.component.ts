c8bea8f8773ce6a7b28f754fe52f615435c6641c
devenkat <38139733+devenkat@users.noreply.github.com>
2020-05-30 17:33:20 -0700

    completed

--- src/app/main/dashboard/cards/week-goals-card/week-goals-card.component.ts
+++ src/app/main/dashboard/cards/week-goals-card/week-goals-card.component.ts
@@ -1,17 +1,26 @@
-import { Component, OnInit, ChangeDetectionStrategy, Input, Output, EventEmitter } from '@angular/core';
-import { WeekGoal } from '../../../../core/store/week-goal/week-goal.model';
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  Input,
+  Output,
+  EventEmitter
+} from "@angular/core";
+import { WeekGoal } from "../../../../core/store/week-goal/week-goal.model";
+import { LoadWeekGoal } from "../../../../core/store/week-goal/week-goal.actions";
 
 /** Displays the weekly goals. */
 @Component({
-  selector: 'app-week-goals-card',
-  templateUrl: './week-goals-card.component.html',
-  styleUrls: ['./week-goals-card.component.scss'],
+  selector: "app-week-goals-card",
+  templateUrl: "./week-goals-card.component.html",
+  styleUrls: ["./week-goals-card.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class WeekGoalsCardComponent implements OnInit {
+  list: string;
 
   // --------------- INPUTS AND OUTPUTS ------------------
-  
+
   /** The start of the week. */
   @Input() startOfWeek: Date;
 
@@ -19,21 +28,33 @@ export class WeekGoalsCardComponent implements OnInit {
   @Input() goals: WeekGoal[];
 
   /** Edit weekly goals events. */
-  @Output() editGoals: EventEmitter<WeekGoal[]> = new EventEmitter<WeekGoal[]>();
+  @Output() editGoals: EventEmitter<WeekGoal[]> = new EventEmitter<
+    WeekGoal[]
+  >();
 
   // --------------- LOCAL UI STATE ----------------------
- 
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    console.log(this.goals);
+  }
+
+  formattedDate() {
+    return (
+      this.startOfWeek.getMonth() +
+      "/" +
+      this.startOfWeek.getDate() +
+      " - " +
+      this.startOfWeek.getMonth() +
+      "/" +
+      (this.startOfWeek.getDate() + 6)
+    );
   }
 
   // --------------- DATA BINDING FUNCTIONS --------------
 
-
   // --------------- EVENT BINDING FUNCTIONS -------------
-
 
   // --------------- OTHER -------------------------------
 }
