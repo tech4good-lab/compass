e12a67838ac63da6c6d291b8c73d29e0d0fd4c49
LeonLin0516 <52585348+LeonLin0516@users.noreply.github.com>
2021-02-16 13:53:38 -0800

    Add Remaining Text Styles (5)

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -4,8 +4,10 @@ import { Routes, RouterModule } from '@angular/router';
 // Containers
 import { ReorientComponent } from './reorient/reorient.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { SchedulingCardComponent } from './reorient/scheduling-card/scheduling-card.component';
 
 const routes: Routes = [
+  { path: 'scheduling-card', component: SchedulingCardComponent },
   { path: 'reorient', component: ReorientComponent },
   { path: 'dashboard', component: DashboardComponent },
 ];
